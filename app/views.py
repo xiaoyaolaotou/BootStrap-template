29befcf85fc775859a74c31cb34713f34c416482
cunzhang <root@localhost.localdomain>
2019-01-09 11:42:24 +0800

    first commit

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request,'base.html')
+
+
+def test(request):
+    return render(request,'test.html')
